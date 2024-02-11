17c1d2e90882cce72bb3b4cd3ea9794f23908921
EC2 Default User <ec2-user@ip-172-31-3-18.ap-northeast-1.compute.internal>
2024-02-11 09:57:00 +0000

    first commit

---
+++ app/controllers/public/sessions_controller.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+class Public::SessionsController < Devise::SessionsController
+  
+  before_action :authenticate_customer!
+  before_action :customer_state, only: [:create]
+  
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+  
+  protected
+  
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
+  end
+  
+  private
+
+  def customer_state
+   customer = Customer.find_by(email: params[:customer][:email])
+   if customer
+    if customer.valid_password?(params[:customer][:password])
+    end
+   end
+  end
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
